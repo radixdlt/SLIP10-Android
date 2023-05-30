639012948fb8d44b352ea7c65d95041f8031c4c1
Jakub Porzuczek <jakub.porzuczek@rdx.works>
2023-05-30 10:18:28 +0200

    remove custom sha-256 implementation

--- src/main/kotlin/com/radixdlt/crypto/hash/sha256/extensions/PublicExtensions.kt
+++ src/main/kotlin/com/radixdlt/crypto/hash/sha256/extensions/PublicExtensions.kt
@@ -1,11 +1,11 @@
 package com.radixdlt.crypto.hash.sha256.extensions
 
-import com.radixdlt.crypto.hash.sha256.Sha256
+import java.security.MessageDigest
 
 /**
  * Returns the SHA256 digest of this byte array.
  */
-fun ByteArray.sha256(): ByteArray = Sha256.digest(this)
+fun ByteArray.sha256(): ByteArray = MessageDigest.getInstance("SHA-256").digest(this)
 
 /**
  * Returns the SHA256 digest of this string.
